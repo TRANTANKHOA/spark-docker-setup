50bdc899076cc3b8d0b040a056b61fc94711823f
Khoa <khoa.tran@rozettatechnology.com>
2021-02-15 16:06:54 +1100

    documentation

--- src/main/scala/spark/streaming/example/Transformations.scala
+++ src/main/scala/spark/streaming/example/Transformations.scala
@@ -1,6 +1,6 @@
 package spark.streaming.example
 
-import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.{Column, DataFrame}
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types.{StringType, StructField, StructType}
 
@@ -44,4 +44,42 @@ object Transformations {
 		"btc_player_data" -> "{\"id\":46049,\"action\":\"view\",\"inserted_at\":\"2020-07-07T07:13:36Z\",\"recorded_time\":1594106015203000,\"file_id\":299442,\"payload\":\"\",\"slide_index\":0,\"ref_slide\":1,\"ref_slide_time\":5,\"user_id\":32213,\"company_id\":22,\"hubshare_id\":null,\"user_agent\":\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36\",\"ip_address\":\"125.63.27.33\",\"session_id\":\"e38cf2b24c59b06a5ba9b8320b0ae235f2b5f28b\",\"client_device_id\":\"waedfb7bc8ed9411937972d55588a38f12b6165c4d8489f78ae9c0d3a896b530a3\",\"client_platform\":\"\",\"client_device_label\":\"Web App V4\",\"client_app_id\":\"\",\"client_version\":\"\",\"client_location\":{\"x\":151.0090863,\"y\":-33.8169516,\"wkb\":\"AQEAAADvKlpvSuBiQBs1ut6R6EDA\",\"srid\":null}}",
 		"relationships_groups" -> "{\"group_id\":100290,\"object_id\":234,\"is_admin\":0,\"type\":\"user\",\"date_added\":\"2018-07-27T10:23:58Z\",\"last_updated\":\"2018-07-30T10:22:52Z\",\"deleted\":1,\"deleted_since\":\"2018-07-30T10:22:52Z\",\"company_id\":3}"
 	)
+
+	/**
+	 * Spark Dataframes are immutable dataset without the concept of primary keys as with RDBMS technologies.
+	 * Therefore, UPSERT can only be done by a left anti join with the existing table which require data shuffling.
+	 * It is recommended that the tables are partitioned/bucketed using the join keys to minimise shuffling.
+	 *
+	 * @param currentRows
+	 * @param newRows
+	 * @param primaryKeys
+	 * @return new dataset with newRows upserted into the currentRows
+	 */
+	def upsertUnion(currentRows: DataFrame, newRows: DataFrame, primaryKeys: Seq[String]): DataFrame = {
+		val newDataWithSchemaCast: DataFrame = selectAndCastColumns(newRows, currentRows)
+		currentRows
+			.join(newDataWithSchemaCast, primaryKeys, joinType = "left_anti")
+			.select(currentRows.columns.map(col): _*)
+			.union(newDataWithSchemaCast)
+	}
+
+	/**
+	 * Ensure that the `newRows` have the same schema as with `currentRows` up to each column's name and data type
+	 *
+	 * @param newRows
+	 * @param currentRows
+	 * @return same data as in newRows but with currentRows's schema
+	 */
+	def selectAndCastColumns(newRows: DataFrame, currentRows: DataFrame): DataFrame = {
+		val newColumnsSet: Set[String] = newRows.columns.toSet
+		val columns: Array[Column] = currentRows.columns.map(column => {
+			if (!newColumnsSet.contains(column)) {
+				lit(null).cast(currentRows.schema(column).dataType) as column
+			} else {
+				newRows(column).cast(currentRows.schema(column).dataType) as column
+			}
+		})
+		newRows.select(columns: _*)
+	}
+
 }
